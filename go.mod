5633035ac830ee715f3a220682b85f8a01a3bea1
Alexey Bavykin <alexey.bavykin@gmail.com>
2020-01-16 04:15:02 +1100

    Add DB

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hiletroy/tg-bot-alexa.git
 go 1.13
 
 require (
+	github.com/lib/pq v1.3.0
 	github.com/pkg/errors v0.9.0 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.0.0-20191225234705-baa616bc00d5
 )
--- main.go
+++ main.go
@@ -1,13 +1,53 @@
-// -*- tab-width: 4; -*-
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
 
 package main
 
 import (
+	"database/sql"
     "log"
     "os"
 
+	_ "github.com/lib/pq"
     tb "gopkg.in/tucnak/telebot.v2"
 )
+
+// func dbFunc(db *sql.DB) gin.HandlerFunc {
+//     return func(c *gin.Context) {
+//         if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+//             c.String(http.StatusInternalServerError,
+//                 fmt.Sprintf("Error creating database table: %q", err))
+//             return
+//         }
+
+//         if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+//             c.String(http.StatusInternalServerError,
+//                 fmt.Sprintf("Error incrementing tick: %q", err))
+//             return
+//         }
+
+//         rows, err := db.Query("SELECT tick FROM ticks")
+//         if err != nil {
+//             c.String(http.StatusInternalServerError,
+//                 fmt.Sprintf("Error reading ticks: %q", err))
+//             return
+//         }
+
+// 		c.String(http.StatusOK, fmt.Sprintf("<pre>ENV: %s\n</pre>", os.Environ()))
+// 		//fmt.Fprintf(os.Stdout, "ENV: %s", os.Environ())
+
+//         defer rows.Close()
+
+//         for rows.Next() {
+//             var tick time.Time
+//             if err := rows.Scan(&tick); err != nil {
+//                 c.String(http.StatusInternalServerError,
+//                     fmt.Sprintf("Error scanning ticks: %q", err))
+//                 return
+//             }
+//             c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+//         }
+//     }
+// }
 
 func main() {
     var (
@@ -32,48 +72,53 @@ func main() {
         log.Fatal(err)
     }
 
-	inlineBtn1 := tb.InlineButton{
-		Unique: "moon",
-		Text:   "Moon 🌚",
-	}
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 
-	inlineBtn2 := tb.InlineButton{
-		Unique: "sun",
-		Text:   "Sun 🌞",
-	}
+    if _, err := db.Exec("CREATE TABLE IF NOT EXISTS messages (id int primary key, messages text, date timestamp)");
+    err != nil {
+        log.Fatal(err)
+    }
 
-	inlineKeys := [][]tb.InlineButton{
-		[]tb.InlineButton{inlineBtn1, inlineBtn2},
-	}
+    inlineBtn1 := tb.InlineButton{
+        Unique: "moon",
+        Text:   "Moon 🌚",
+    }
 
-	b.Handle(&inlineBtn1, func(c *tb.Callback) {
+    inlineBtn2 := tb.InlineButton{
+        Unique: "sun",
+        Text:   "Sun 🌞",
+    }
+
+    inlineKeys := [][]tb.InlineButton{
+        []tb.InlineButton{inlineBtn1, inlineBtn2},
+    }
+
+    b.Handle(&inlineBtn1, func(c *tb.Callback) {
         // Required for proper work
-		b.Respond(c, &tb.CallbackResponse{
-			ShowAlert: false,
-		})
+        b.Respond(c, &tb.CallbackResponse{
+            ShowAlert: false,
+        })
         // Send messages here
-		b.Send(c.Sender, "Moon says 'Hi'!")
-	})
+        b.Send(c.Sender, "Moon says 'Hi'!")
+    })
 
-	b.Handle(&inlineBtn2, func(c *tb.Callback) {
-		b.Respond(c, &tb.CallbackResponse{
-			ShowAlert: false,
-		})
-		b.Send(c.Sender, "Sun says 'Hi'!")
-	})
+    b.Handle(&inlineBtn2, func(c *tb.Callback) {
+        b.Respond(c, &tb.CallbackResponse{
+            ShowAlert: false,
+        })
+        b.Send(c.Sender, "Sun says 'Hi'!")
+    })
 
-	b.Handle("/hello", func(m *tb.Message) {
+    b.Handle("/hello", func(m *tb.Message) {
         b.Send(m.Sender, "You entered "+m.Text+" ("+m.Payload+")")
     })
 
-	b.Handle("/pick_time", func(m *tb.Message) {
-		b.Send(
-			m.Sender,
-			"Day or night, you choose",
-			&tb.ReplyMarkup{InlineKeyboard: inlineKeys})
-	})
+    b.Handle("/pick_time", func(m *tb.Message) {
+        b.Send(
+            m.Sender,
+            "Day or night, you choose",
+            &tb.ReplyMarkup{InlineKeyboard: inlineKeys})
+    })
 
     b.Start()
 }
-
-// (setq indent-tabs-mode nil)
